41d4f9c7f29c35e4bd14c462ff4494d5e3f19baf
Alex Ferrer <alexfc.14@gmail.com>
2022-04-25 12:46:08 +0200

    9x6 chessboard

--- src/cv.py
+++ src/cv.py
@@ -4,13 +4,13 @@ import glob
 
 import streamlit as st
 
-def calibrate(images, verbose=True):
+def calibrate(images, grid_w=9, grid_h=6, verbose=True):
     # 1. DETECT KEYPOINTS
     # termination criteria
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6*7,3), np.float32)
-    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(grid_h,5,0)
+    objp = np.zeros((grid_h*grid_w,3), np.float32)
+    objp[:,:2] = np.mgrid[0:grid_w,0:grid_h].T.reshape(-1,2)
     # Arrays to store object points and image points from all the images.
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
@@ -21,14 +21,14 @@ def calibrate(images, verbose=True):
             st.image(img, width=100)
         calibration_shape = calibration_shape or gray.shape[::-1]
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+        ret, corners = cv.findChessboardCorners(gray, (grid_w,grid_h), cv.CALIB_CB_FAST_CHECK | cv.CALIB_CB_ADAPTIVE_THRESH)
         # If found, add object points, image points (after refining them)
         if ret == True:
             objpoints.append(objp)
             corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
             imgpoints.append(corners)
             # Draw and display the corners
-            cv.drawChessboardCorners(img, (7,6), corners2, ret)
+            cv.drawChessboardCorners(img, (grid_w,grid_h), corners2, ret)
             if verbose:
                 st.image(img)
 
