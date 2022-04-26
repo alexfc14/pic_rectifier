82d6359ae7568584d58dffe39ed1fcd10dcf778c
Alex Ferrer <alexfc.14@gmail.com>
2022-04-26 02:31:32 +0200

    rearrange

--- src/calibrate.py
+++ src/calibrate.py
@@ -3,8 +3,8 @@ import numpy as np
 from PIL import Image
 from cv import calibrate, undistort
 
-if 'count' not in st.session_state:
-    st.session_state.calibration_images = []
+if 'calibration_images' not in st.session_state:
+    st.session_state.calibration_images = {}
     st.session_state.distorted_images = {}
     st.session_state.undistorted_images = {}
     st.params = None
@@ -17,27 +17,29 @@ for f in calibration_files:
     image = Image.open(f)
     img_array = np.array(image)
     
-    st.session_state.calibration_images += [img_array]
+    st.session_state.calibration_images[f.name] = img_array
 
 # st.write("filenames:", st.session_state.files)
 
 if calibration_files:
-    st.text('start calibrating')
-    params = calibrate(st.session_state.calibration_images)
-    st.text('finished calibrating')
-    st.session_state.params = params
 
     ## upload files calibration pics
     distorted_files = st.file_uploader("Upload images to undistort", accept_multiple_files=True)
-    for f in distorted_files:
-        image = Image.open(f)
-        img_array = np.array(image)
-        print('distorted image', f.name)
-        st.session_state.distorted_images[f.name] = img_array
-        st.image(img_array)
+    if distorted_files:
+        st.text('start calibrating')
+        params = calibrate(st.session_state.calibration_images)
+        st.text('finished calibrating')
+        st.session_state.params = params
+    
+        for f in distorted_files:
+            image = Image.open(f)
+            img_array = np.array(image)
+            print('distorted image', f.name)
+            st.session_state.distorted_images[f.name] = img_array
+            st.image(img_array)
 
-    for f in distorted_files:
-        img_array = st.session_state.distorted_images[f.name]
-        undistorted = undistort(img_array, *params)
-        st.session_state.undistorted_images[f.name] = undistorted
-        st.image(undistorted)
+        for f in distorted_files:
+            img_array = st.session_state.distorted_images[f.name]
+            undistorted = undistort(img_array, *params)
+            st.session_state.undistorted_images[f.name] = undistorted
+            st.image(undistorted)
--- src/cv.py
+++ src/cv.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2 as cv
-import glob
 
 import streamlit as st
 
@@ -15,7 +14,9 @@ def calibrate(images, grid_w=9, grid_h=6, verbose=True):
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
     calibration_shape = None
-    for img in images:
+    for name, img in images.items():
+        if verbose:
+            st.text(name)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         if verbose:
             st.image(img, width=100)
@@ -31,20 +32,51 @@ def calibrate(images, grid_w=9, grid_h=6, verbose=True):
             cv.drawChessboardCorners(img, (grid_w,grid_h), corners2, ret)
             if verbose:
                 st.image(img)
+        else:
+            st.text(name + ' failed!')
 
     # 2. CALIBRATE
     retval, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, calibration_shape, None, None)
+    print('camera matrix\n', mtx)
+    print(f'returned error  {retval:.2f} pixels')
+    if verbose:
+        st.text(f'error  {retval:.2f} pixels')
     return mtx, dist
+
+def reprojection_error(objpoints, imgpoints, rvecs, tvecs, mtx, dist):
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+        mean_error += error
+        print('err', error)
+    mean_error /= len(objpoints)
+    print(f"total error: {mean_error}")
+    return mean_error
 
 def undistort(img, mtx, dist):
     # refine camera matrix
     h,  w = img.shape[:2]
     newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-    
-    # undistort
+    undistort
     dst = cv.undistort(img, mtx, dist, None, newcameramtx)
     # crop the image
     x, y, w, h = roi
     dst = dst[y:y+h, x:x+w]
     
+    # do not refine
+    # dst = cv.undistort(img, mtx, dist, None, None)
     return dst
+
+if __name__ == '__main__':
+    import os
+    from PIL import Image
+    images = {}
+    path = '../data/example/good/'
+    for fp in os.listdir(path):
+        with open('../data/example/good/' + fp, 'rb') as f:
+            image = Image.open(f)
+            img_array = np.array(image)
+            images[fp] = img_array
+    params = calibrate(images, verbose=False)
+    undistorted = undistort(img_array, *params)
